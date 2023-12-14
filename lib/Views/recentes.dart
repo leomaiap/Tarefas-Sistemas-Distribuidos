329b647a8d2a18a9035c05a232b0187c6dca53e0
Leonardo Maia <leomaiapleo@gmail.com>
2023-12-14 18:36:16 -0300

    Day selection no Recentes

--- lib/Views/recentes.dart
+++ lib/Views/recentes.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:planner/Autenticador/login.dart';
+import 'package:planner/Widgets/dayWidget.dart';
 
 class Recentes extends StatefulWidget {
   const Recentes({super.key});
@@ -10,6 +11,19 @@ class Recentes extends StatefulWidget {
 }
 
 class _RecentesState extends State<Recentes> {
+  late DateTime today;
+  int daySelected = 0;
+  @override
+  void initState() {
+    super.initState();
+    setState(() {
+      today = DateTime.now();
+    });
+  }
+
+  bool isSelected(int index) {
+    return daySelected == index;
+  }
   // Para saber qual usuario atual use UserSession.getID()
   @override
   Widget build(BuildContext context) {
@@ -31,8 +45,43 @@ class _RecentesState extends State<Recentes> {
         ],
         centerTitle: true,
       ),
-      body: Text("Implementar as tarefas mais urgentes dos proximos 5 dias 'Views/recentes.dart'"),
-      // Implementar escolher 5 dias
+      body: Container(
+        child: Column(
+          children: [
+            Container(
+              height: 130,
+              padding: EdgeInsets.all(12),
+              child: GridView.builder(
+                itemCount: 5,
+              gridDelegate:  SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 5,
+                crossAxisSpacing: 0,
+              ),
+              itemBuilder:(context, index) {
+                return GestureDetector(
+                  onTap: () {
+                    daySelected = index;
+                    print(daySelected);
+                    setState(() {
+                      
+                    });
+                  },
+                  child: Container(
+                    child: DayCard(
+                      day: today.add(Duration(days: index)), 
+                      isSelected: isSelected(index)),
+                  ),
+                );
+              },
+              ),
+            ),
+            //Lista de tarefas aqui
+            Container(
+              //ListView.builder()
+            )
+          ],
+        ),
+      )
       // consultar o banco de dados referente ao usuari0 e a data, e listar as tarefas daquele dia (listview.builder() + gesturedetector())
     );
   }
---
+++ lib/Widgets/dayWidget.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class DayCard extends StatelessWidget {
+  DateTime day;
+  bool isSelected;
+  DayCard({super.key, required this.day, required this.isSelected});
+
+  @override
+  Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    if (isSelected){
+      return Container(
+          decoration: BoxDecoration(
+          color: Colors.blueGrey,
+          borderRadius: BorderRadius.all(Radius.circular(8))
+        ),
+        child: Container(
+          child:Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("${DateFormat.MMM().format(day)}", style: TextStyle(
+                fontSize: screenWidth*0.03,
+                color: Colors.white
+              ),),
+              Text("${day.day}",  style: TextStyle(
+                fontSize: screenWidth*0.05,
+                fontWeight: FontWeight.bold,
+                color: Colors.white
+              ),),
+              Text("${DateFormat.E().format(day)}", style: TextStyle(
+                fontSize: screenWidth*0.03,
+                color: Colors.white))
+            ],
+          )
+        ),
+          
+      );
+    }
+    else{
+      return Container(
+        decoration: BoxDecoration(
+        color: Colors.transparent,
+        borderRadius: BorderRadius.all(Radius.circular(8))
+      ),
+      child: Container(
+        child:Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("${DateFormat.MMM().format(day)}", style: TextStyle(
+              fontSize: screenWidth*0.03,
+              
+            ),),
+            Text("${day.day}",  style: TextStyle(
+              fontSize: screenWidth*0.05,
+              fontWeight: FontWeight.bold,
+              
+            ),),
+            Text("${DateFormat.E().format(day)}")
+          ],
+        )
+      ),
+    );
+    }
+    
+  }
+}
