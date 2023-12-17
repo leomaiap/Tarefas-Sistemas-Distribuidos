a214c15d49db89d1750cbdf726efcc410dd0f764
Guilherme <gpaes@id.uff.br>
2023-12-17 02:06:32 -0300

    Form de criação de tarefa atualizado para usar widget 'Time Picker'

--- lib/Views/newTask.dart
+++ lib/Views/newTask.dart
@@ -16,6 +16,23 @@ class NewTask extends StatefulWidget {
 
 
 class _NewTaskState extends State<NewTask> {
+
+  List<Color> colorsList = [
+    Color(0xFFD7423D),
+    Color(0xFFFFE066),
+    Color(0xFFFFBA59),
+    Color(0xFFFF8C8C),
+    Color(0xFFFF99E5),
+    Color(0xFFC3A6FF),
+    Color(0xFF9FBCF5),
+    Color(0xFF8CE2FF),
+    Color(0xFF87F5B5),
+    Color(0xFFBCF593),
+    Color(0xFFE2F587),
+    Color(0xFFD9BCAD),
+    Colors.grey.shade50
+  ];
+
   TextEditingController nomeController = TextEditingController();
   TextEditingController startController = TextEditingController();
   TextEditingController endController = TextEditingController();
@@ -39,21 +56,15 @@ class _NewTaskState extends State<NewTask> {
     return "  Data: " + date;
   }
 
-  List<Color> colorsList = [
-    Color(0xFFD7423D),
-    Color(0xFFFFE066),
-    Color(0xFFFFBA59),
-    Color(0xFFFF8C8C),
-    Color(0xFFFF99E5),
-    Color(0xFFC3A6FF),
-    Color(0xFF9FBCF5),
-    Color(0xFF8CE2FF),
-    Color(0xFF87F5B5),
-    Color(0xFFBCF593),
-    Color(0xFFE2F587),
-    Color(0xFFD9BCAD),
-    Colors.grey.shade50
-    ];
+  String getStartDateDisplay(){
+    if(startTimeVazio){return "  Start Time: Não selecionado ainda.";}
+    return "  Start Time: " + startTime;
+  }
+
+  String getEndDateDisplay(){
+    if(endTimeVazio){return "  End Time: Não selecionado ainda.";}
+    return "  End Time: " + endTime;
+  }
 
   void updateDate(DateTime? dt){
     if(dt==null){return;}
@@ -62,11 +73,40 @@ class _NewTaskState extends State<NewTask> {
       date = dt.toString().split(' ')[0];
     });
   }
+
+  void updateStart(TimeOfDay? dt){
+    if(dt==null){return;}
+    setState(() {
+      startTimeVazio = false;
+      startTime = dt.toString().split('(')[1].replaceAll(')', '');
+    });
+  }
+
+  void updateEnd(TimeOfDay? dt){
+    if(dt==null){return;}
+    setState(() {
+      endTimeVazio = false;
+      endTime = dt.toString().split('(')[1].replaceAll(')', '');
+    });
+  }
+
   void _showDatePicker(){
-    print('hyyyy');
     showDatePicker(context: context, initialDate: DateTime.now(),
         firstDate: DateTime(2000), lastDate:  DateTime(2030)).then((value) => updateDate(value));
   }
+
+  void _showStartPicker(){
+    showTimePicker(context: context, initialTime:  TimeOfDay.now()).then(
+            (value) => updateStart(value)
+    );
+  }
+
+  void _showEndPicker(){
+    showTimePicker(context: context, initialTime:  TimeOfDay.now()).then(
+            (value) => updateEnd(value)
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     //double screenWidth = MediaQuery.of(context).size.width;
@@ -100,6 +140,8 @@ class _NewTaskState extends State<NewTask> {
             mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
+
+              //NOME DA TAREFA
               TextFormField(
                 keyboardType: TextInputType.text,
                 decoration: InputDecoration(
@@ -121,6 +163,8 @@ class _NewTaskState extends State<NewTask> {
                 },
               ),
               const SizedBox(height: 20,),
+
+              //PICK DATE
               Center(
                 child: SizedBox(
                   width:335,
@@ -139,60 +183,78 @@ class _NewTaskState extends State<NewTask> {
                   ),
                 ),
               ),
+              Center(
+                child: Text(getDateDisplay(),
+                  style: TextStyle(
+                    fontSize: 21,
+                    fontWeight: FontWeight.bold,
 
-              Text(getDateDisplay(),
-                style: TextStyle(
-                  fontSize: 21,
-                  fontWeight: FontWeight.bold,
-
+                  ),
                 ),
               ),
               const SizedBox(height: 10,),
 
-              TextFormField(
-                keyboardType: TextInputType.text,
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Colors.white,
-                  hintText: "Start Time",
+              //PICK START TIME
+              Center(
+                child: SizedBox(
+                  width:335,
+                  child: MaterialButton(onPressed: (){_showStartPicker();},
+                    child: Padding(
+                      padding: EdgeInsets.all(4),
+                      child: Text("Pick Start Time",
+                          style: TextStyle(
+                              color:Colors.white,
+                              fontSize:25
+                          )
+                      ),
+                    ),
+                    color: colorsList[widget.color],
+
+                  ),
                 ),
-                style: TextStyle(
-                    fontSize: 25,
+              ),
+              Center(
+                child: Text(getStartDateDisplay(),
+                  style: TextStyle(
+                    fontSize: 18,
                     fontWeight: FontWeight.bold,
-                    color: const Color.fromARGB(255, 0, 0, 0)),
-                controller: startController,
-                onChanged: (String value) {
-                  startTime = value;
-                  //print(nome);
-                  setState(() {
-                    startTimeVazio = value.isEmpty;
-                  });
-                },
+
+                  ),
+                ),
               ),
               const SizedBox(height: 10,),
 
-              TextFormField(
-                keyboardType: TextInputType.text,
-                decoration: const InputDecoration(
-                  filled: true,
-                  fillColor: Colors.white,
-                  hintText: "End Time",
+              //PICK END TIME
+              Center(
+                child: SizedBox(
+                  width:335,
+                  child: MaterialButton(onPressed: (){_showEndPicker();},
+                    child: Padding(
+                      padding: EdgeInsets.all(4),
+                      child: Text("Pick End Time",
+                          style: TextStyle(
+                              color:Colors.white,
+                              fontSize:25
+                          )
+                      ),
+                    ),
+                    color: colorsList[widget.color],
+
+                  ),
                 ),
-                style: const TextStyle(
-                    fontSize: 25,
+              ),
+              Center(
+                child: Text(getEndDateDisplay(),
+                  style: TextStyle(
+                    fontSize: 18,
                     fontWeight: FontWeight.bold,
-                    color: Color.fromARGB(255, 0, 0, 0)),
-                controller: endController,
-                onChanged: (String value) {
-                  endTime = value;
-                  //print(nome);
-                  setState(() {
-                    endTimeVazio = value.isEmpty;
-                  });
-                },
+
+                  ),
+                ),
               ),
               const SizedBox(height: 25,),
 
+              //WRITE NOTE
               const Text("  Nota sobre tarefa:",
               style: TextStyle(
                 fontSize: 21,
@@ -216,9 +278,9 @@ class _NewTaskState extends State<NewTask> {
                   note = value;
                 },
               ),
-
               const SizedBox(height: 35,),
 
+              //CRIAR TAREFA
               Center(
                 child: ElevatedButton(onPressed: (nomeVazio || endTimeVazio || startTimeVazio || dateVazio)
                       ? null
